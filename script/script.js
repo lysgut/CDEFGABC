16119f0df52c7565395d6848a517938e44d2ede2
lysgut <ltuninetticivarolo@escuelasproa.edu.ar>
2023-07-25 12:20:24 -0300

    sss

--- script/script.js
+++ script/script.js
@@ -1,1 +1,6 @@
-console.log('esto si anda')
+const playBTN = document.getElementById("play-btn");
+const synth = new Tone.Synth().toDestination();
+const now = Tone.now()
+playBTN.addEventListener("click", () => {
+    console.log('entro')
+})
