8e42855d0e7b20fdb70fd722cd1a84c5ced2fdc4
tiagoo <thurstaltamirano@escuelasproa.edu.ar>
2023-07-31 13:36:27 -0300

    la tecka hace sonido :0

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
-const playBTN = document.getElementById("play-btn");
+let Synth = import("https://unpkg.com/tone@14.7.77/build/eTone.js"); 
 const synth = new Tone.Synth().toDestination();
-const now = Tone.now()
-playBTN.addEventListener("click", () => {
-    console.log('entro')
-})
+
+const Do = document.getElementById('doBlanca');
+Do.addEventListener('click', async () =>{
+    await Tone.start();
+    synth.triggerAttackRelease("C4", "8n");
+    }
+)
